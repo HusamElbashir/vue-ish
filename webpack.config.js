0aa1e3692c543760e7ecb8c5b1686b6fd944a7b8
Husam Ibrahim <huielbashir@gmail.com>
2019-07-28 21:10:48 +0300

    lint webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: ['./main.js'],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: ['./main.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 }
