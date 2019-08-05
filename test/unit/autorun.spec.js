32bc8f8a657eb73253828d3dde1ff1ddb4449c56
Husam Ibrahim <huielbashir@gmail.com>
2019-08-05 12:04:34 +0300

    add autoRun unit test

---
+++ test/unit/autorun.spec.js
@@ -0,0 +1,38 @@
+import autoRun from '@/autorun'
+import Dep from '@/dep'
+
+describe('autoRun', () => {
+  const queue = []
+  const spyCb = jest.fn(() => queue.push(spyCb))
+  const setter = val => queue.push(val)
+  const propertyDescriptor = Object.getOwnPropertyDescriptor(Dep, 'activeComputation')
+
+  beforeAll(() => {
+    Object.defineProperty(Dep, 'activeComputation', {
+      configurable: true,
+      enumerable: true,
+      set: setter,
+    })
+  })
+
+  afterAll(() => {
+    Object.defineProperty(Dep, 'activeComputation', propertyDescriptor)
+  })
+
+  it('sets activeComputation, calls cb then clears activeComputation', () => {
+    const wrappedCb = autoRun(spyCb)
+    expect(queue).toStrictEqual([wrappedCb, spyCb, null])
+  })
+
+  // makes sure wrappedCb and cb are equivalent (wrappedCb calls cb).
+  // This is because deps actually collect wrappedCb as a subscriber
+  // and then invoke it when they're mutated.
+  it('wrappedCb invokes cb when called', () => {
+    const wrappedCb = autoRun(spyCb)
+    spyCb.mockClear()
+
+    expect(spyCb).not.toHaveBeenCalled()
+    wrappedCb()
+    expect(spyCb).toHaveBeenCalled()
+  })
+})
