28341db54fd320695f29444a457d3884abe19d05
Husam Ibrahim <huielbashir@gmail.com>
2019-08-08 04:54:31 +0300

    chore: fix comment location in autorun.js

--- src/autorun.js
+++ src/autorun.js
@@ -1,11 +1,11 @@
 import Dep from './dep'
 import observe from './observe'
 
-// wraps a function call and sets itself
-// as the activeComputation to be collected
-// as a subscriber by any reactive dependencies
-// accessed by the function call.
 export default function autoRun(cb) {
+  // wraps a function call and sets itself
+  // as the activeComputation to be collected
+  // as a subscriber by any reactive dependencies
+  // accessed by the function call.
   function wrappedCb() {
     Dep.activeComputation = wrappedCb
     cb()
