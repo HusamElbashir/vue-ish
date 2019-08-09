02a651a81d16a5b60e30aabb578b4cf70d4b7d6e
Husam Ibrahim <huielbashir@gmail.com>
2019-08-09 07:25:48 +0300

    test: add e2e test

---
+++ test/e2e/reactivity.spec.js
@@ -0,0 +1,22 @@
+import observe from '@/observe'
+import autoRun from '@/autoRun'
+
+describe('basic reactivity', () => {
+  it('reacts', () => {
+    const el = document.createElement('span')
+
+    expect(el.textContent).toBe('')
+
+    const state = observe({ message: '' })
+
+    autoRun(() => {
+      el.textContent = state.message
+    })
+
+    expect(el.textContent).toBe('')
+
+    state.message = 'foo'
+
+    expect(el.textContent).toBe('foo')
+  })
+})
